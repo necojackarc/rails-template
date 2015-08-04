02c923d075e75ed7581346a37e460ccfb4068f13
necojackarc <necojackarc@gmail.com>
2015-08-04 15:36:03 +0900

    Bugfix

--- template.rb
+++ template.rb
@@ -53,7 +53,7 @@ I18n.enforce_available_locales = true
         view_specs: false,
         helper_specs: false,
         routing_specs: false,
-        controller_specs: true,
+        controller_specs: true
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
 
